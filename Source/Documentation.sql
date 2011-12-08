5f04e306b428a51640b052a6d23b026cca3331d1
Admin <admin@chrisoldwood.com>
2011-12-08 21:20:58 +0000

    Initital version.

---
+++ Source/Documentation.sql
@@ -0,0 +1,31 @@
+/**
+ * \file
+ * \brief  The main body of documentation.
+ * \author Chris Oldwood
+ */
+
+/**
+ * \mainpage SQL Server Schema Cop
+ *
+ * \section introduction Introduction
+ *
+ * SS-Cop is a static analysis...
+ *
+ * \note
+ * This documentation was generated directly from the SS-Cop T-SQL source code
+ * using Doxygen and the sql2doxygen input filter.
+ * See <a href="http://www.cix.co.uk/~gort/sql.htm">http://www.cix.co.uk/~gort/sql.htm</a>
+ *
+ * \note
+ * The unit tests for SS-Cop are also written in T-SQL using SS-Unit (SQL Server Unit),
+ * a T-SQL unit test framework itself written in T-SQL.
+ * See <a href="http://www.cix.co.uk/~gort/sql.htm">http://www.cix.co.uk/~gort/sql.htm</a>
+ */
+
+/**
+ * \namespace sscop
+ * \brief     The schema used for all SS-Cop objects.
+ *
+ * SS-Cop uses its own schema so that its objects will not conflict with your
+ * application.
+ */
